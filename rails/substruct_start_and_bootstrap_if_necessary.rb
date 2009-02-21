c68cf008372ebc977e3a96898b020bbd4f696a91
unknown <rogerpack2005@gmail.com>
2009-02-21 11:45:39 -0700

    substruct tests attempt to preload gems

--- rails/substruct_start_and_bootstrap_if_necessary.rb
+++ rails/substruct_start_and_bootstrap_if_necessary.rb
@@ -1,8 +1,16 @@
+# currently we need a lotta gems
+# require them up front just in case they're not installed
+require 'rubygems'
+
+for gem in ["RedCloth", "fastercsv", "mime/types", "mini_magick", "ezcrypto"] do
+ require gem
+end
+
 Dir.chdir 'substruct'
 
 ENV['RAILS_ENV'] = 'production'
 if RUBY_PLATFORM =~ /mswin|mingw/
-  require(File.join(File.dirname(__FILE__), 'config', 'boot')) # windows weirdness
+  require(File.join(File.dirname(__FILE__), 'config', 'boot')) # windows weirdness--or is it a 1.8.6 weirdness?
 else
   require(File.join(File.dirname(__FILE__), 'substruct', 'config', 'boot'))
 end
